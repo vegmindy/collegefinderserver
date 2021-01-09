a0bfee51fba1e73f024d00727f25390512626aea
vegmindy <72030094+vegmindy@users.noreply.github.com>
2021-01-09 14:18:21 -0500

    second attempt to deploy

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,7 @@ router.post('/signup', async (req, res) => {
             email,
             password: bcrypt.hashSync(password, 13),
             currentState,
-            role: role || 'user',
+            role: role || 'student',
         })
         const token = jwt.sign({id: newUser.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24})
         res.status(201).json({
--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const {Sequelize} = require('sequelize');
 
 const db = new Sequelize(process.env.DATABASE_URL, {
-    // host: 'localhost',
     dialect: 'postgres'
 });
 
