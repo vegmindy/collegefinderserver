e3fd169e324a2c340f17cc5608f912d83719f14f
vegmindy <72030094+vegmindy@users.noreply.github.com>
2021-01-09 13:18:38 -0500

    first deploy

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
-// const {Sequelize} = require('sequelize');
+const {Sequelize} = require('sequelize');
 
-// const db = new Sequelize(process.env.DB_CONNECTION_STRING, {
-//     host: 'localhost',
-//     dialect: 'postgres'
-// });
-
-// module.exports = db;
-
-const {Sequelize} = require("sequelize");
-
-const db = new Sequelize(process.env.DB_CONNECTION_STRING);
+const db = new Sequelize(process.env.DATABASE_URL, {
+    // host: 'localhost',
+    dialect: 'postgres'
+});
 
 module.exports = db;
+
+// const {Sequelize} = require("sequelize");
+
+// const db = new Sequelize(process.env.DB_CONNECTION_STRING);
+
+// module.exports = db;
